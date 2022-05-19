1f3228112583fd78d4e6f68c4880120f52dc8a09
sergiu128 <sergiu.marin@talostrading.com>
2022-05-19 11:56:21 +0200

    Sync tls client and server

---
+++ examples/async_adapter/tls/certs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p certs
+rm certs/*
+
+echo "server cert"
+openssl req -new -nodes -x509 -out certs/server.pem -keyout certs/server.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=www.random.com/emailAddress=sergiu@talos.com"
+
+echo "client cert"
+openssl req -new -nodes -x509 -out certs/client.pem -keyout certs/client.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=www.random.com/emailAddress=sergiu@talos.com"
+
--- examples/async_adapter/tls/server.go
+++ examples/async_adapter/tls/server.go
@@ -1,5 +1,83 @@
 package main
 
-func main() {
+import (
+	"crypto/rand"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"io"
+	"net"
+)
 
+var (
+	certPath = flag.String("cert", "./certs/server.pem", "server certificate path")
+	keyPath  = flag.String("key", "./certs/server.key", "server private key path")
+	addr     = flag.String("addr", ":8080", "tls server address")
+)
+
+func main() {
+	flag.Parse()
+
+	cert, err := tls.LoadX509KeyPair(*certPath, *keyPath)
+	if err != nil {
+		panic(err)
+	}
+
+	cfg := tls.Config{
+		Certificates: []tls.Certificate{cert},
+		Rand:         rand.Reader,
+	}
+	ln, err := tls.Listen("tcp", *addr, &cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("listening")
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			conn.Close()
+			panic(err)
+		}
+
+		fmt.Println("accepted conn", conn.RemoteAddr())
+
+		_, ok := conn.(*tls.Conn)
+		if ok {
+			fmt.Println("client is using TLS")
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	buf := make([]byte, 4096)
+	for {
+		fmt.Println("conn waiting to read")
+		n, err := conn.Read(buf)
+		if err != nil {
+			if err != io.EOF {
+				panic(err)
+			} else {
+				break
+			}
+		}
+
+		buf = buf[:n]
+		fmt.Println("read", string(buf))
+		fmt.Println("echoing")
+
+		n, err = conn.Write(buf)
+		if err != nil {
+			if err != io.EOF {
+				panic(err)
+			} else {
+				break
+			}
+		}
+	}
+
+	fmt.Println("conn", conn.RemoteAddr(), "closed")
 }
