c56a9ab45dfc21aae8a7089c1c735d3577185c03
sergiu128 <sergiu.marin@talostrading.com>
2022-06-13 18:37:59 +0200

    Fix reading zero length websocket payload

--- sonicwebsocket/websocket.go
+++ sonicwebsocket/websocket.go
@@ -165,20 +165,20 @@ func (s *WebsocketStream) asyncReadFrameMask(b []byte, cb sonic.AsyncCallback) {
 }
 
 func (s *WebsocketStream) asyncReadPayload(b []byte, cb sonic.AsyncCallback) {
-	if payloadLen := int64(s.readFrame.Len()); payloadLen > 0 {
-		if remaining := payloadLen - int64(cap(b)); remaining > 0 {
-			cb(ErrPayloadTooBig, 0)
-		} else {
-			s.async.AsyncReadAll(b[:payloadLen], func(err error, n int) {
-				if err != nil {
-					cb(err, n)
-				} else {
-					cb(nil, n)
-				}
-			})
+	if pl := s.readFrame.Len(); pl > 0 {
+		if payloadLen := int64(pl); payloadLen > 0 {
+			if remaining := payloadLen - int64(cap(b)); remaining > 0 {
+				cb(ErrPayloadTooBig, 0)
+			} else {
+				s.async.AsyncReadAll(b[:payloadLen], func(err error, n int) {
+					if err != nil {
+						cb(err, n)
+					} else {
+						cb(nil, n)
+					}
+				})
+			}
 		}
-	} else {
-		panic(fmt.Errorf("invalid uint64 to int conversion uint64(payloadLen)=%v", s.readFrame.Len()))
 	}
 }
 
