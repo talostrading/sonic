b1b3700abe991d8e4e015a34c729c9f0ea20880b
sergiu128 <sergiu.marin@talostrading.com>
2022-06-16 11:43:29 +0200

    Rename dispatch to post

--- examples/post/post.go
+++ examples/post/post.go
@@ -13,7 +13,7 @@ func main() {
 		// this copy is needed, otherwise we will dispatch 10, the last value of i, each time
 		j := i
 		ioc.Post(func() {
-			fmt.Println("dispatched: ", j)
+			fmt.Println("posted: ", j)
 		})
 	}
 
