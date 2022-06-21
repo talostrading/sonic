d634664381e205a40996fae9a0456b1c8ce40d4f
sergiu128 <sergiu.marin@talostrading.com>
2022-06-21 15:07:04 +0200

    Add autobahn testsuite scaffolding

---
+++ autobahn/client.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strconv"
+
+	"github.com/talostrading/sonic"
+	"github.com/talostrading/sonic/sonicwebsocket"
+)
+
+var (
+	addr     = flag.String("addr", "ws://localhost:9001", "server address")
+	testCase = flag.Int("case", -1, "autobahn test case to run")
+)
+
+func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("panicked - updating reports before rethrowing")
+			updateReports()
+			panic(err)
+		}
+	}()
+
+	flag.Parse()
+
+	n := getCaseCount()
+
+	if *testCase == -1 {
+		fmt.Printf("running against all %d cases\n", n)
+		for i := 1; i <= n; i++ {
+			runTest(i)
+		}
+		updateReports()
+	} else {
+		if *testCase < 1 || *testCase > n {
+			panic(fmt.Errorf("invalid test case %d; min=%d max=%d", *testCase, 1, n))
+		} else {
+			fmt.Printf("running against test case %d\n", *testCase)
+			runTest(*testCase)
+			updateReports()
+		}
+	}
+}
+
+func getCaseCount() int {
+	ioc := sonic.MustIO()
+	defer ioc.Close()
+
+	stream, err := sonicwebsocket.NewWebsocketStream(ioc, nil, sonicwebsocket.RoleClient)
+	if err != nil {
+		panic(err)
+	}
+	defer stream.Close(sonicwebsocket.Normal, "")
+
+	err = stream.Handshake(*addr + "/getCaseCount")
+	if err != nil {
+		panic(err)
+	}
+	b := make([]byte, 128)
+	n, err := stream.Read(b)
+	if err != nil {
+		panic(err)
+	}
+	b = b[:n]
+	nn, err := strconv.ParseInt(string(b), 10, 32)
+	if err != nil {
+		panic(err)
+	}
+
+	return int(nn)
+}
+
+func runTest(i int) {
+	fmt.Printf("running test case %d\n", i)
+	ioc := sonic.MustIO()
+
+	addr := *addr + fmt.Sprintf("/runCase?case=%d&agent=sonic", i)
+
+	stream, err := sonicwebsocket.NewWebsocketStream(ioc, nil, sonicwebsocket.RoleClient)
+	if err != nil {
+		panic(err)
+	}
+
+	stream.AsyncHandshake(addr, func(err error) {
+		if err != nil {
+			stream.Close(sonicwebsocket.Normal, "")
+			ioc.Close()
+			panic(err)
+		} else {
+			b := make([]byte, 4096)
+			stream.AsyncRead(b, func(err error, n int) {
+				if err != nil {
+					stream.Close(sonicwebsocket.Normal, "")
+					ioc.Close()
+					panic(err)
+				} else {
+					b = b[:n]
+					stream.AsyncWrite(b, func(err error, n int) {
+						if err != nil {
+							panic(err)
+						}
+						stream.Close(sonicwebsocket.Normal, "")
+						ioc.Close()
+					})
+				}
+			})
+		}
+	})
+
+	ioc.Run()
+}
+
+func updateReports() {
+	fmt.Println("updating reports")
+	ioc := sonic.MustIO()
+
+	stream, err := sonicwebsocket.NewWebsocketStream(ioc, nil, sonicwebsocket.RoleClient)
+	if err != nil {
+		panic("could not update reports")
+	}
+
+	stream.AsyncHandshake(*addr+"/updateReports?agent=sonic", func(err error) {
+		if err != nil {
+			panic("could not update reports")
+		} else {
+			stream.Close(sonicwebsocket.Normal, "")
+			ioc.Close()
+		}
+	})
+
+	ioc.Run()
+}
---
+++ autobahn/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -it --rm -v "${PWD}/config:/config" -v "${PWD}/reports:/reports" -p 9001:9001 --name fuzzingserver crossbario/autobahn-testsuite
