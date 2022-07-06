807688d0df7040548d8badeb8cbf04c665b90bb2
sergiu128 <sergiu.marin@talostrading.com>
2022-07-06 19:42:40 +0100

    Add soft mutex

---
+++ sonicwebsocket/soft_mutex.go
@@ -0,0 +1,52 @@
+package sonicwebsocket
+
+import "fmt"
+
+type softMutex struct {
+	id int
+}
+
+func (s *softMutex) Reset() {
+	s.id = 0
+}
+
+func (s *softMutex) IsLocked() bool {
+	return s.id != 0
+}
+
+func (s *softMutex) IsLockedWith(op operation) bool {
+	return s.id == op.ID()
+}
+
+func (s *softMutex) Lock(op operation) {
+	if s.id != 0 {
+		panic(fmt.Errorf("attempting to lock operation=%d while already locked on op=%d", op.ID(), s.id))
+	}
+	s.id = op.ID()
+}
+
+func (s *softMutex) Unlock(op operation) {
+	if s.id != op.ID() {
+		panic(fmt.Errorf("attempting to unlock operation=%d while locked on a different op=%d", op.ID(), s.id))
+	}
+	s.id = 0
+}
+
+func (s *softMutex) TryLock(op operation) bool {
+	if s.id == op.ID() {
+		panic(fmt.Errorf("trying to lock an already locked op=%d", op.ID()))
+	}
+	if s.id != 0 {
+		return false
+	}
+	s.id = op.ID()
+	return true
+}
+
+func (s *softMutex) TryUnlock(op operation) bool {
+	if s.id != op.ID() {
+		return false
+	}
+	s.id = 0
+	return true
+}
---
+++ sonicwebsocket/soft_mutex_test.go
@@ -0,0 +1,120 @@
+package sonicwebsocket
+
+import (
+	"testing"
+)
+
+type testOp struct {
+	id int
+}
+
+func (t testOp) ID() int {
+	return t.id
+}
+
+var _ operation = testOp{}
+
+var op1 = &testOp{1}
+var op2 = &testOp{2}
+
+func TestMutex(t *testing.T) {
+	s := &softMutex{}
+
+	s.Lock(op1)
+	if s.id != 1 {
+		t.Fatalf("wrong id expected=%d given=%d", op1.id, s.id)
+	}
+
+	s.Unlock(op1)
+	if s.id != 0 {
+		t.Fatalf("wrong id expected=%d given=%d", 0, s.id)
+	}
+
+	if ok := s.TryLock(op1); !ok {
+		t.Fatalf("should have locked")
+	} else if s.id != op1.id {
+		t.Fatalf("wrong id expected=%d given=%d", op1.id, s.id)
+	}
+
+	if ok := s.TryLock(op2); ok {
+		t.Fatalf("should not have locked")
+	} else if s.id != op1.id {
+		t.Fatalf("wrong id expected=%d given=%d", op1.id, s.id)
+	}
+
+	if ok := s.TryUnlock(op2); ok {
+		t.Fatalf("should not have unlocked")
+	} else if s.id != op1.id {
+		t.Fatalf("wrong id expected=%d given=%d", op1.id, s.id)
+	}
+
+	if ok := s.TryUnlock(op1); !ok {
+		t.Fatalf("should have unlocked")
+	} else if s.id != 0 {
+		t.Fatalf("wrong id expected=%d given=%d", 0, s.id)
+	}
+}
+
+func TestSoftMutexLockPanic(t *testing.T) {
+	valid := false
+
+	defer func() {
+		if !valid {
+			t.Fatalf("should have panicked")
+		}
+	}()
+
+	defer func() {
+		if err := recover(); err != nil {
+			valid = true
+		}
+	}()
+
+	s := &softMutex{}
+
+	s.Lock(op1)
+	s.Lock(op1)
+}
+
+func TestSoftMutexTryLockPanic(t *testing.T) {
+	valid := false
+
+	defer func() {
+		if !valid {
+			t.Fatalf("should have panicked")
+		}
+	}()
+
+	defer func() {
+		if err := recover(); err != nil {
+			valid = true
+		}
+	}()
+
+	s := &softMutex{}
+
+	s.TryLock(op1)
+	s.TryLock(op1)
+}
+
+func TestSoftMutexUnlockPanic(t *testing.T) {
+	valid := false
+
+	defer func() {
+		if !valid {
+			t.Fatalf("should have panicked")
+		}
+	}()
+
+	defer func() {
+		if err := recover(); err != nil {
+			valid = true
+		}
+	}()
+
+	s := &softMutex{}
+
+	s.Lock(op1)
+	s.Unlock(op1)
+	s.Unlock(op2)
+}
