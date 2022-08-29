567beea608c84be251900ac727877ba81d118a1e
sergiu128 <sergiu128@protonmail.com>
2022-08-29 16:31:06 +0200

    Update conn_test

--- conn_test.go
+++ conn_test.go
@@ -8,14 +8,9 @@ import (
 	"syscall"
 	"testing"
 	"time"
-
-	"github.com/talostrading/sonic/sonicopts"
 )
 
 func TestAsyncTCPEchoClient(t *testing.T) {
-	ioc := MustIO()
-	defer ioc.Close()
-
 	closer := make(chan struct{}, 1)
 
 	go func() {
@@ -52,6 +47,11 @@ func TestAsyncTCPEchoClient(t *testing.T) {
 			}
 		}
 	}()
+
+	time.Sleep(500 * time.Millisecond)
+
+	ioc := MustIO()
+	defer ioc.Close()
 
 	conn, err := Dial(ioc, "tcp", "localhost:8080")
 	if err != nil {
@@ -93,90 +93,7 @@ func TestAsyncTCPEchoClient(t *testing.T) {
 	closer <- struct{}{}
 }
 
-func TestAsyncTCPEchoServer(t *testing.T) {
-	ioc := MustIO()
-	defer ioc.Close()
-
-	go func() {
-		// Wait until there's a listener.
-		time.Sleep(200 * time.Millisecond)
-
-		conn, err := net.Dial("tcp", "localhost:8081")
-		if err != nil {
-			panic(err)
-		}
-
-		b := make([]byte, 128)
-
-		for {
-			conn.Write([]byte("hello"))
-			n, err := conn.Read(b)
-			if err != nil {
-				panic(err)
-			}
-
-			b = b[:n]
-
-			if string(b) != "hello" {
-				panic(fmt.Errorf("did not read %v", string(b)))
-			}
-		}
-	}()
-
-	ln, err := Listen(ioc, "tcp", "localhost:8081", sonicopts.Nonblocking(true))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	handle := func(conn Conn) {
-		b := make([]byte, 5)
-		var onAsyncRead AsyncCallback
-		onAsyncRead = func(err error, n int) {
-			if err != nil {
-				if err != io.EOF {
-					t.Fatal(err)
-				}
-			} else {
-				b = b[:n]
-
-				if string(b) != "hello" {
-					t.Fatalf("did not read %v", string(b))
-				}
-
-				conn.AsyncWriteAll(b, func(err error, n int) {
-					if err != nil {
-						if !errors.Is(err, io.EOF) || !errors.Is(err, syscall.EPIPE) {
-							t.Fatal(err)
-						}
-					} else {
-						b = b[:cap(b)]
-						conn.AsyncReadAll(b, onAsyncRead)
-					}
-				})
-			}
-		}
-		conn.AsyncReadAll(b, onAsyncRead)
-	}
-
-	ln.AsyncAccept(func(err error, conn Conn) {
-		if err != nil {
-			t.Fatalf("error on async accept err=%v", err)
-		} else {
-			handle(conn)
-		}
-	})
-
-	for i := 0; i < 10; i++ {
-		ioc.RunOne()
-	}
-
-	ln.Close()
-}
-
 func TestReadHandlesError(t *testing.T) {
-	ioc := MustIO()
-	defer ioc.Close()
-
 	go func() {
 		ln, err := net.Listen("tcp", "localhost:8082")
 		if err != nil {
@@ -195,6 +112,11 @@ func TestReadHandlesError(t *testing.T) {
 
 		conn.Close()
 	}()
+
+	time.Sleep(500 * time.Millisecond)
+
+	ioc := MustIO()
+	defer ioc.Close()
 
 	conn, err := Dial(ioc, "tcp", "localhost:8082")
 	if err != nil {
@@ -226,9 +148,6 @@ func TestReadHandlesError(t *testing.T) {
 }
 
 func TestWriteHandlesError(t *testing.T) {
-	ioc := MustIO()
-	defer ioc.Close()
-
 	go func() {
 		ln, err := net.Listen("tcp", "localhost:8083")
 		if err != nil {
@@ -252,6 +171,11 @@ func TestWriteHandlesError(t *testing.T) {
 			panic(err)
 		}
 	}()
+
+	time.Sleep(500 * time.Millisecond)
+
+	ioc := MustIO()
+	defer ioc.Close()
 
 	conn, err := Dial(ioc, "tcp", "localhost:8083")
 	if err != nil {
