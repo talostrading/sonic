485841a5b2d74521166899bfd289add7fc10012d
sergiu128 <sergiu128@protonmail.com>
2022-10-24 18:43:33 +0200

    Change ErrNeedMore

--- sonicerrors/errors.go
+++ sonicerrors/errors.go
@@ -6,5 +6,5 @@ var (
 	ErrWouldBlock = errors.New("operation would block")
 	ErrCancelled  = errors.New("operation cancelled")
 	ErrTimeout    = errors.New("operation timed out")
-	ErrNeedMore   = errors.New("need to read/write more bytes")
+	ErrNeedMore   = errors.New("need more bytes")
 )
