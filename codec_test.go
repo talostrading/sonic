df376cf0c2943bd366eee9049ab40abc745e9334
sergiu128 <sergiu.marin@talostrading.com>
2023-02-12 18:13:29 +0100

    Test NonblockingCodecConn reads

--- codec_test.go
+++ codec_test.go
@@ -1,7 +1,10 @@
 package sonic
 
 import (
+	"github.com/talostrading/sonic/sonicerrors"
+	"net"
 	"testing"
+	"time"
 )
 
 var _ Codec[TestItem, TestItem] = &TestCodec{}
@@ -46,6 +49,146 @@ func TestSimpleCodec(t *testing.T) {
 		t.Fatal(err)
 	}
 	for i := 0; i < len(item.V); i++ {
+		if item.V[i] != byte(i+1) {
+			t.Fatal("wrong decoding")
+		}
+	}
+}
+
+func setupCodecTestWriter() chan struct{} {
+	mark := make(chan struct{}, 1)
+	go func() {
+		ln, err := net.Listen("tcp", "localhost:9090")
+		if err != nil {
+			panic(err)
+		}
+		defer func() {
+			ln.Close()
+			mark <- struct{}{}
+		}()
+		mark <- struct{}{}
+
+		conn, err := ln.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		<-mark // send a partial
+		n, err := conn.Write([]byte{1, 2, 3})
+		if err != nil {
+			panic(err)
+		}
+		if n != 3 {
+			panic("did not write 3")
+		}
+
+		<-mark // send the rest and one extra byte
+		n, err = conn.Write([]byte{
+			4,
+			5,
+			6 /* this is extra and should show up in the src buffer */})
+		if err != nil {
+			panic(err)
+		}
+		if n != 3 {
+			panic("did not write 3")
+		}
+	}()
+	return mark
+}
+
+func TestNonblockingCodecConnAsyncReadNext(t *testing.T) {
+	mark := setupCodecTestWriter()
+	defer func() { <-mark /* wait for the listener to close*/ }()
+	<-mark // wait for the listener to open
+
+	ioc := MustIO()
+	defer ioc.Close()
+
+	conn, err := Dial(ioc, "tcp", "localhost:9090")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	src := NewByteBuffer()
+	dst := NewByteBuffer()
+	codecConn, err := NewNonblockingCodecConn[TestItem, TestItem](
+		conn, &TestCodec{}, src, dst)
+
+	codecConn.AsyncReadNext(func(err error, item TestItem) {
+		if err != nil {
+			t.Fatal(err)
+		} else {
+			for i := 0; i < 5; i++ {
+				if item.V[i] != byte(i+1) {
+					t.Fatal("wrong decoding")
+				}
+			}
+		}
+	})
+
+	// we need 2 reads for a full decode
+	mark <- struct{}{} // write first partial chunk
+	ioc.RunOne()       // read it
+
+	mark <- struct{}{} // write second chunk + extra byte
+	ioc.RunOne()       // read it
+
+	if src.WriteLen() != 1 {
+		t.Fatal("should have read the extra byte (6)")
+	}
+}
+
+func TestNonblockingCodecConnReadNext(t *testing.T) {
+	mark := setupCodecTestWriter()
+	defer func() { <-mark /* wait for the listener to close*/ }()
+	<-mark // wait for the listener to open
+
+	ioc := MustIO()
+	defer ioc.Close()
+
+	conn, err := Dial(ioc, "tcp", "localhost:9090")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	src := NewByteBuffer()
+	dst := NewByteBuffer()
+	codecConn, err := NewNonblockingCodecConn[TestItem, TestItem](
+		conn, &TestCodec{}, src, dst)
+
+	item, err := codecConn.ReadNext()
+	if err != sonicerrors.ErrWouldBlock {
+		t.Fatal("should block")
+	}
+	for _, v := range item.V {
+		if v != 0 {
+			t.Fatal("failed read did not return an empty item")
+		}
+	}
+
+	// we need 2 reads for a full decode
+	mark <- struct{}{}                // write first partial chunk
+	time.Sleep(10 * time.Millisecond) // wait for the other goroutine to write
+	item, err = codecConn.ReadNext()
+	if err != sonicerrors.ErrWouldBlock {
+		// we got the first chunk but that's a partial and a subsequent
+		// read should just block
+		t.Fatal("should block")
+	}
+	for _, v := range item.V {
+		if v != 0 {
+			t.Fatal("failed read did not return an empty item")
+		}
+	}
+
+	mark <- struct{}{}                // write second chunk + extra byte
+	time.Sleep(10 * time.Millisecond) // wait for the other goroutine to write
+	item, err = codecConn.ReadNext()
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i := 0; i < 5; i++ {
 		if item.V[i] != byte(i+1) {
 			t.Fatal("wrong decoding")
 		}
