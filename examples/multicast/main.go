c15f810aa745c5bef5a29a4cc4733f2366ecde13
Sergiu <sergiu.marin@talostrading.com>
2023-03-07 10:58:05 +0100

    Track loop latency for example udp client

--- examples/multicast/main.go
+++ examples/multicast/main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/talostrading/sonic"
+	"github.com/talostrading/sonic/util"
+	"log"
 	"net"
 )
 
-// for a server: iperf -c 224.0.1.0 -u -i 1
+var (
+	// for a server: iperf -c 224.0.1.0 -u -i 1
+	addr = flag.String("addr", "224.0.1.0:5001", "multicast group to join")
 
-var addr = flag.String("addr", "224.0.1.0:5001", "multicast group to join")
+	ns = flag.Int("ns", 128, "number of samples")
+)
 
 func main() {
+	flag.Parse()
+
 	ioc := sonic.MustIO()
 	defer ioc.Close()
 
@@ -19,6 +25,9 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	log.Printf(
+		"interface name=%s mtu=%d index=%d addr=%s\n",
+		iff.Name, iff.MTU, iff.Index, iff.HardwareAddr)
 
 	mc, err := sonic.NewUDPMulticastClient(ioc, iff, net.IPv4zero)
 	if err != nil {
@@ -41,12 +50,22 @@ func main() {
 			panic(err)
 		} else {
 			b = b[:n]
-			fmt.Println(string(b))
+			log.Println(string(b))
 			b = b[:cap(b)]
 			mc.AsyncReadFrom(b, onRead)
 		}
 	}
 	mc.AsyncReadFrom(b, onRead)
 
-	ioc.Run()
+	tracker := util.NewTrackerWithSamples(*ns)
+	for {
+		start := util.GetMonoNanos()
+		n, _ := ioc.PollOne()
+		if n > 0 {
+			end := util.GetMonoNanos()
+			if stats := tracker.Record(end - start); stats != nil {
+				log.Printf("loop latency (ns): %s\n", stats)
+			}
+		}
+	}
 }
---
+++ util/tracker.go
@@ -0,0 +1,102 @@
+package util
+
+import (
+	"fmt"
+	"math"
+)
+
+/*
+#include <time.h>
+static unsigned long long get_nanos(void) {
+	struct timespec ts;
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+	return (unsigned long long)ts.tv_sec * 1000000000UL + ts.tv_nsec;
+}
+*/
+import "C"
+
+func GetMonoNanos() int64 {
+	return int64(C.get_nanos())
+}
+
+type TrackerStats struct {
+	Min    float64
+	Avg    float64
+	Max    float64
+	StdDev float64
+	Var    float64
+}
+
+func (s *TrackerStats) String() string {
+	return fmt.Sprintf(
+		"min/avg/max/stddev/var = %f/%f/%f/%f/%f",
+		s.Min, s.Avg, s.Max, s.StdDev, s.Var)
+}
+
+func (s *TrackerStats) Reset() {
+	s.Min = math.MaxFloat64
+	s.Avg = 0.0
+	s.Max = 0.0
+	s.StdDev = 0.0
+	s.Var = 0.0
+}
+
+type Tracker struct {
+	stats   *TrackerStats
+	N       int
+	samples []int64
+	ix      int
+}
+
+func NewTracker() *Tracker {
+	return NewTrackerWithSamples(128)
+}
+
+func NewTrackerWithSamples(n int) *Tracker {
+	return &Tracker{
+		N:       n,
+		samples: make([]int64, n),
+		stats:   &TrackerStats{},
+	}
+}
+
+func (t *Tracker) Record(sample int64) *TrackerStats {
+	if t.ix >= len(t.samples) {
+		t.stats.Reset()
+
+		mean, stddev, variance := 0.0, 0.0, 0.0
+
+		for _, s := range t.samples {
+			sf := float64(s)
+
+			if sf < t.stats.Min {
+				t.stats.Min = sf
+			}
+			if sf > t.stats.Max {
+				t.stats.Max = sf
+			}
+			mean += sf
+		}
+		mean /= float64(len(t.samples))
+
+		for _, s := range t.samples {
+			diff := float64(s) - mean
+			stddev += diff * diff
+		}
+		stddev /= float64(len(t.samples))
+		variance = stddev
+		stddev = math.Sqrt(stddev)
+
+		t.stats.Avg = mean
+		t.stats.StdDev = stddev
+		t.stats.Var = variance
+
+		t.ix = 0
+		return t.stats
+	}
+
+	t.samples[t.ix] = sample
+	t.ix++
+
+	return nil
+}
