f229312eee7e551a051b57a1c81fca9a460e4d6a
sergiu128 <sergiu.marin@talostrading.com>
2023-03-13 16:25:50 +0100

    Test mono time

--- util/mono_test.go
+++ util/mono_test.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 	"time"
 )
@@ -22,4 +23,37 @@ func BenchmarkTimeNow(b *testing.B) {
 	}
 	b.ReportAllocs()
 	fmt.Println(s)
+}
+
+func TestMono(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		startMono := GetMonoNanos()
+
+		sleepingFor := time.Duration(rand.Intn(100_000)) * time.Microsecond
+
+		start := time.Now()
+		var diff time.Duration
+		for {
+			diff = time.Now().Sub(start)
+			if diff > sleepingFor {
+				break
+			}
+		}
+
+		diffMono := GetMonoNanos() - startMono
+		if diffMono < sleepingFor.Nanoseconds() {
+			t.Fatalf(
+				"did not sleep long enough sleeping_for=%s diff=%d",
+				sleepingFor, diff)
+		}
+
+		delta := diffMono - diff.Nanoseconds()
+		if delta < 0 {
+			delta = -delta
+		}
+
+		if delta > 100_000 { // 100 micros
+			t.Fatalf("mono and time.Now() difference not the same diff=%d", delta)
+		}
+	}
 }
