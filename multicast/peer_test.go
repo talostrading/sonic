37be0a22976f6eaf9de72e3a3357ad055743d907
Sergiu <sergiu.marin@talostrading.com>
2023-04-22 13:19:56 +0200

    Separate bsd multicast peer tests

--- multicast/peer_test.go
+++ multicast/peer_test.go
@@ -290,42 +290,6 @@ func TestUDPPeer_Join(t *testing.T) {
 	}
 }
 
-func TestUDPPeer_SetOutboundInterfaceOnUnspecifiedIPandPort(t *testing.T) {
-	ioc := sonic.MustIO()
-	defer ioc.Close()
-
-	peer, err := NewUDPPeer(ioc, "udp", "")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer peer.Close()
-
-	if err := peer.SetOutboundIPv4("en0"); err != nil {
-		t.Fatal(err)
-	}
-
-	outboundInterface, outboundIP := peer.Outbound()
-	fmt.Println("outbound", outboundInterface, outboundIP)
-}
-
-func TestUDPPeer_SetOutboundInterfaceOnUnspecifiedPort(t *testing.T) {
-	ioc := sonic.MustIO()
-	defer ioc.Close()
-
-	peer, err := NewUDPPeer(ioc, "udp", "localhost:0")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer peer.Close()
-
-	if err := peer.SetOutboundIPv4("en0"); err != nil {
-		t.Fatal(err)
-	}
-
-	outboundInterface, outboundIP := peer.Outbound()
-	fmt.Println("outbound", outboundInterface, outboundIP)
-}
-
 func TestUDPPeer_SetLoop1(t *testing.T) {
 	ioc := sonic.MustIO()
 	defer ioc.Close()
@@ -359,5 +323,24 @@ func TestUDPPeer_SetLoop1(t *testing.T) {
 	}
 	if peer.Loop() {
 		t.Fatal("peer should not loop packets")
+	}
+}
+
+func TestUDPPeer_ListInterfaces(t *testing.T) {
+	iffs, err := net.Interfaces()
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println("found", len(iffs), "interfaces")
+	for _, iff := range iffs {
+		fmt.Printf(
+			"interface name=%s index=%d mac=%s up=%v loopback=%v multicast=%v\n",
+			iff.Name,
+			iff.Index,
+			iff.HardwareAddr,
+			iff.Flags&net.FlagUp != 0,
+			iff.Flags&net.FlagLoopback != 0,
+			iff.Flags&net.FlagMulticast != 0,
+		)
 	}
 }
