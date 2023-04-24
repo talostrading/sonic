5f84ea405873e49eca68df63fcaff6199753346c
Sergiu <sergiu.marin@talostrading.com>
2023-04-24 11:49:23 +0200

    Socket unbind from device

--- socket_test.go
+++ socket_test.go
@@ -19,13 +19,20 @@ func TestSocket_BindToDeviceIPv4(t *testing.T) {
 	}
 
 	for _, iff := range iffs {
-		log.Printf("attempting to bind socket to interface=%s", iff.Name)
+		log.Printf("attempting to bind socket to device=%s", iff.Name)
 		if err := sock.BindToDevice(iff.Name); err != nil {
-			t.Fatalf("could not bind socket to lo0 err=%v", err)
+			t.Fatalf("could not bind socket to %s err=%v", iff.Name, err)
 		} else if sock.BoundInterface() == nil {
-			t.Fatal("should have set bound interface")
+			t.Fatal("should have set bound device")
 		} else {
-			log.Printf("bound socket fd=%d to interface=%s", sock.RawFd(), iff.Name)
+			log.Printf("bound socket fd=%d to device=%s", sock.RawFd(), iff.Name)
+		}
+
+		log.Printf("attempting to unbind socket from device=%s", iff.Name)
+		if err := sock.UnbindFromDevice(); err != nil {
+			t.Fatalf("could not unbind socket from %s err=%v", iff.Name, err)
+		} else {
+			log.Printf("unbound socket fd=%d from device=%s", sock.RawFd(), iff.Name)
 		}
 	}
 }
