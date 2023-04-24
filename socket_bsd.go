4c9df563fd1d2d19760ff8a71add8899861f8b8b
Sergiu <sergiu.marin@talostrading.com>
2023-04-24 11:49:23 +0200

    Socket unbind from device

--- socket_bsd.go
+++ socket_bsd.go
@@ -30,3 +30,29 @@ func (s *Socket) BindToDevice(name string) error {
 		return fmt.Errorf("cannot yet bind to device when domain is ipv6")
 	}
 }
+
+func (s *Socket) UnbindFromDevice() error {
+	if s.boundInterface == nil {
+		return nil
+	}
+
+	if s.domain == SocketDomainIPv4 {
+		_, _, errno := syscall.Syscall6(
+			uintptr(syscall.SYS_SETSOCKOPT),
+			uintptr(s.fd),
+			uintptr(syscall.IPPROTO_IP),
+			uintptr(syscall.IP_BOUND_IF),
+			0, 0, 0,
+		)
+		if errno != 0 {
+			var err error
+			err = errno
+			return err
+		} else {
+			s.boundInterface = nil
+			return nil
+		}
+	} else {
+		return fmt.Errorf("cannot yet bind to device when domain is ipv6")
+	}
+}
--- socket_linux.go
+++ socket_linux.go
@@ -5,6 +5,7 @@ package sonic
 import (
 	"net"
 	"syscall"
+	"unsafe"
 )
 
 func (s *Socket) BindToDevice(name string) error {
@@ -24,4 +25,37 @@ func (s *Socket) BindToDevice(name string) error {
 		s.boundInterface = iff
 		return nil
 	}
+}
+
+func (s *Socket) UnbindFromDevice() error {
+	if s.boundInterface == nil {
+		return nil
+	}
+
+	_, _, errno := syscall.Syscall6(
+		uintptr(syscall.SYS_SETSOCKOPT),
+		uintptr(s.fd),
+		uintptr(syscall.SOL_SOCKET),
+		uintptr(syscall.SO_BINDTODEVICE),
+		uintptr(unsafe.Pointer(&[]byte("_")[0])),
+		0, 0,
+	)
+	if errno != 0 {
+		var err error
+		err = errno
+		return err
+	} else {
+		s.boundInterface = nil
+		return nil
+	}
+	//if err := syscall.SetsockoptString(
+	//	s.fd,
+	//	syscall.SOL_SOCKET,
+	//	syscall.SO_BINDTODEVICE,
+	//	"",
+	//); err != nil {
+	//} else {
+	//	s.boundInterface = nil
+	//	return nil
+	//}
 }
