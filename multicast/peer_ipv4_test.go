d41ba91d184b60faff4183fd4c3cd61c8ae4c584
Sergiu <sergiu.marin@talostrading.com>
2023-04-25 12:56:38 +0200

    Add test for multiple readers none joined

--- multicast/peer_ipv4_test.go
+++ multicast/peer_ipv4_test.go
@@ -1598,3 +1598,85 @@ func TestUDPPeerIPv4_JoinReadBlockUnblockRead(t *testing.T) {
 		t.Fatal("did not read anything after unblocking")
 	}
 }
+
+func TestUDPPeerIPv4_MultipleReadersNoneJoin(t *testing.T) {
+	ioc := sonic.MustIO()
+	defer ioc.Close()
+
+	multicastIP := "224.0.1.0"
+	multicastPort := 1234
+	multicastAddr, err := netip.ParseAddrPort(fmt.Sprintf("%s:%d", multicastIP, multicastPort))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	readers := make(map[*UDPPeer]struct {
+		index int
+		nRead int
+		from  map[netip.AddrPort]struct{}
+	})
+	for i := 0; i < 10; i++ {
+		r, err := NewUDPPeer(ioc, "udp", fmt.Sprintf(":%d", multicastPort))
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer r.Close()
+
+		init := struct {
+			index int
+			nRead int
+			from  map[netip.AddrPort]struct{}
+		}{
+			index: i,
+			nRead: 0,
+			from:  make(map[netip.AddrPort]struct{}),
+		}
+		readers[r] = init
+
+		b := make([]byte, 128)
+		var onRead func(error, int, netip.AddrPort)
+		onRead = func(err error, n int, from netip.AddrPort) {
+			if err == nil {
+				entry := readers[r]
+				entry.nRead++
+				entry.from[from] = struct{}{}
+				readers[r] = entry
+
+				r.AsyncRead(b, onRead)
+			}
+		}
+		r.AsyncRead(b, onRead)
+	}
+
+	w, err := NewUDPPeer(ioc, "udp", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer w.Close()
+
+	var onWrite func(error, int)
+	onWrite = func(err error, n int) {
+		if err == nil {
+			w.AsyncWrite([]byte("hello"), multicastAddr, onWrite)
+		}
+	}
+	w.AsyncWrite([]byte("hello"), multicastAddr, onWrite)
+
+	start := time.Now()
+	now := time.Now()
+	for now.Sub(start).Seconds() < 10 {
+		now = time.Now()
+		ioc.PollOne()
+	}
+
+	log.Printf("done")
+
+	for _, reader := range readers {
+		log.Printf("reader index=%d n_read=%d from=%+v", reader.index, reader.nRead, reader.from)
+		if reader.nRead != 0 || len(reader.from) != 0 {
+			t.Fatalf(
+				"none of the readers joined, but reader %d received %d times from %+v",
+				reader.index, reader.nRead, reader.from)
+		}
+	}
+}
