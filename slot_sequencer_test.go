f8169dfb8152b99359980fbf4a85405e21a5213e
sergiu128 <sergiu4096@gmail.com>
2023-05-30 16:32:29 +0200

    Offset slots popped with Pop(seq)

--- slot_sequencer_test.go
+++ slot_sequencer_test.go
@@ -8,7 +8,7 @@ func setupSlotSequencerTest(
 	t *testing.T, xs []byte,
 ) (*SlotSequencer,
 	*ByteBuffer) {
-	s, err := NewSlotSequencer()
+	s, err := NewSlotSequencer(len(xs))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -111,22 +111,109 @@ func TestSlotSequencerPush3(t *testing.T) {
 
 func TestSlotSequencerPop0(t *testing.T) {
 	s, b := setupSlotSequencerTest(t, []byte{0, 1, 2, 3, 4})
-	for i := 0; i < 5; i++ {
-		s.Push(i, b.Save(1))
+	for seq := 0; seq < 5; seq++ {
+		s.Push(seq, b.Save(1))
 	}
 
-	for i := 0; i < 5; i++ {
-		slot, ok := s.Pop(i)
+	for seq := 0; seq < 5; seq++ {
+		slot, ok := s.Pop(seq)
 		if !ok {
 			t.Fatal("not popped")
 		}
-		if slot.Length != 1 || slot.Index != i {
+		if slot.Length != 1 || slot.Index != 0 {
 			t.Fatal("wrong slot")
 		}
 	}
 
 	if s.Size() != 0 {
 		t.Fatal("wrong size")
+	}
+}
+
+func TestSlotSequencerPop1(t *testing.T) {
+	// Pop in sequenced order. That means each slot following a popped one must
+	// be offset on the next pop. That also means each popped slot has Index 0.
+
+	s, b := setupSlotSequencerTest(t, []byte{0, 1, 2, 3, 4})
+	for seq := 10; seq < 15; seq++ {
+		s.Push(seq, b.Save(1))
+	}
+
+	for seq := 10; seq < 15; seq++ {
+		slot, _ := s.Pop(seq)
+		if slot.Index != 0 {
+			t.Fatal("wrong index for slot")
+		}
+		discarded := b.Discard(slot)
+		if slot.Length != discarded {
+			t.Fatal("wrong Discard return")
+		}
+	}
+
+	if s.Size() != 0 {
+		t.Fatal("slot sequencer should be empty")
+	}
+}
+
+func TestSlotSequencerPop2(t *testing.T) {
+	// Pop in reverse sequenced order. That means none of the slots will be
+	// offset.
+
+	s, b := setupSlotSequencerTest(t, []byte{0, 1, 2, 3, 4})
+	for seq := 10; seq < 15; seq++ {
+		s.Push(seq, b.Save(1))
+	}
+
+	expectedIndex := 4
+	for seq := 14; seq >= 10; seq-- {
+		slot, _ := s.Pop(seq)
+		if slot.Index != expectedIndex {
+			t.Fatal("wrong index for slot")
+		}
+		expectedIndex--
+		discarded := b.Discard(slot)
+		if slot.Length != discarded {
+			t.Fatal("wrong Discard return")
+		}
+	}
+
+	if s.Size() != 0 {
+		t.Fatal("slot sequencer should be empty")
+	}
+}
+
+func TestSlotSequencerPop3(t *testing.T) {
+	// Pop randomly.
+
+	s, b := setupSlotSequencerTest(t, []byte{0, 1, 2, 3, 4})
+	for seq := 10; seq < 15; seq++ {
+		s.Push(seq, b.Save(1))
+	}
+
+	permutation := []int{11, 13, 10, 12, 14}
+	//	{ 1,  3,  0,  2,  4},      none popped
+	//	{-1,  2,  0,  1,  3},      11 popped. 12, 13, 14 offset by 1 = 11.Length
+	//	{-1, -1,  0,  1,  2},      13 popped. 14 offset by 1 = 13.Length
+	//	{-1, -1, -1,  0,  1},      10 popped. 12, 14 offset by 1 = 10.Length
+	//	{-1, -1, -1, -1,  0},      12 popped. 14 offset by 1 = 12.Length
+	//	{-1, -1, -1, -1, -1},      14 popped. done.
+	expectedIndices := []int{1, 2, 0, 0, 0}
+	for i := 0; i < len(permutation); i++ {
+		seq := permutation[i]
+		slot, _ := s.Pop(seq)
+		discarded := b.Discard(slot)
+		if slot.Length != discarded {
+			t.Fatal("wrong Discard return")
+		}
+		if slot.Index != expectedIndices[i] {
+			t.Fatalf(
+				"wrong index given=%d expected=%d",
+				slot.Index, expectedIndices[i])
+		}
+	}
+
+	if s.Size() != 0 {
+		t.Fatal("slot sequencer should be empty")
 	}
 }
 
