a49ce5c8a53899ec58b28f01869ebfe077debc9b
sergiu128 <sergiu4096@gmail.com>
2023-06-13 20:23:07 +0200

    Fenwick tree benchmarks

--- util/fenwick_tree_test.go
+++ util/fenwick_tree_test.go
@@ -1,6 +1,8 @@
 package util
 
 import (
+	"fmt"
+	"io"
 	"testing"
 )
 
@@ -124,4 +126,70 @@ func TestFenwickTreeClear(t *testing.T) {
 	if tree.Sum() != 0 {
 		t.Fatal("wrong sum")
 	}
+}
+
+func BenchmarkFenwickTreeAdd(b *testing.B) {
+	// TODO trash the cache with a linked list
+
+	tree := NewFenwickTree(1024 * 1024 * 16)
+	for i := 0; i < b.N; i++ {
+		tree.Add(i%tree.Size(), i)
+	}
+	fmt.Fprint(io.Discard, tree.Sum())
+	b.ReportAllocs()
+}
+
+func BenchmarkFenwickTreePrefixSum(b *testing.B) {
+	// TODO trash the cache with a linked list
+
+	const N = 1024 * 10
+
+	b.Run("fenwick_tree_prefix_sum", func(b *testing.B) {
+		tree := NewFenwickTree(N)
+		for i := 0; i < tree.Size(); i++ {
+			tree.Add(i, i)
+		}
+		total := 0
+		for i := 0; i < b.N; i++ {
+			total += tree.SumUntil(i % tree.Size())
+		}
+		fmt.Fprint(io.Discard, total)
+		b.ReportAllocs()
+	})
+
+	b.Run("array_linear_prefix_sum", func(b *testing.B) {
+		var xs [N]int
+		fn := func(index int) int {
+			s := 0
+			for i := 0; i <= index; i++ {
+				s += xs[i]
+			}
+			return s
+		}
+		total := 0
+		for i := 0; i < b.N; i++ {
+			total += fn(i % N)
+		}
+		fmt.Fprint(io.Discard, total)
+		b.ReportAllocs()
+	})
+}
+
+func BenchmarkFenwickTreeRangeSum(b *testing.B) {
+	// Here we are doing two SumUntil, so it should be 2x of PrefixSum.
+	// TODO trash the cache with a linked list
+
+	tree := NewFenwickTree(1024 * 1024 * 16)
+	for i := 0; i < tree.Size(); i++ {
+		tree.Add(i, i)
+	}
+	total := 0
+	for i := 0; i < b.N; i++ {
+		total += tree.SumRange(
+			i%tree.Size(),
+			(i%tree.Size()+10)%tree.Size(),
+		)
+	}
+	fmt.Fprint(io.Discard, total)
+	b.ReportAllocs()
 }
