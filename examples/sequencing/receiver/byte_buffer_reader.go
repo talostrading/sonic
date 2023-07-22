5588c7ac0ddb03312ed397d5c8ac4a89a709a7bd
sergiu128 <sergiu4096@gmail.com>
2023-07-22 15:46:25 +0200

    Organize sequencing example code

---
+++ examples/sequencing/receiver/byte_buffer_reader.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"net/netip"
+	"time"
+
+	"github.com/HdrHistogram/hdrhistogram-go"
+	"github.com/talostrading/sonic"
+	"github.com/talostrading/sonic/multicast"
+	"github.com/talostrading/sonic/util"
+)
+
+var _ Reader = &ByteBufferReader{}
+
+type ByteBufferReader struct {
+	proc        ByteBufferProcessor
+	b           *sonic.ByteBuffer
+	peer        *multicast.UDPPeer
+	readBufSize int
+	hist        *hdrhistogram.Histogram
+	sofar       int
+	first       bool
+}
+
+func NewByteBufferReader(
+	proc ByteBufferProcessor,
+	bufSize, readBufSize int,
+	peer *multicast.UDPPeer,
+) *ByteBufferReader {
+	r := &ByteBufferReader{
+		proc:        proc,
+		peer:        peer,
+		readBufSize: readBufSize,
+		b:           sonic.NewByteBuffer(),
+		hist:        hdrhistogram.New(1, 10_000_000, 1),
+	}
+
+	r.b.Reserve(bufSize)
+	r.b.Zero()
+	log.Printf(
+		"created sonic byte_buffer size=%s",
+		util.ByteCountSI(int64(bufSize)),
+	)
+
+	return r
+}
+
+func (r *ByteBufferReader) Setup() {
+	decode := func() (seq, n int, payload []byte) {
+		seq = int(binary.BigEndian.Uint32(r.b.Data()[:4]))
+		n = int(binary.BigEndian.Uint32(r.b.Data()[4:]))
+		if n > *readBufferSize {
+			panic(fmt.Errorf(
+				"something wrong as n = %d > %d b.Data()[hex]=%s",
+				n,
+				*readBufferSize,
+				hex.EncodeToString(r.b.Data())),
+			)
+		}
+		r.b.Consume(8)
+		payload = r.b.Data()[:n]
+		return
+	}
+
+	var onRead func(error, int, netip.AddrPort)
+	onRead = func(err error, n int, _ netip.AddrPort) {
+		if err != nil {
+			panic(err)
+		} else {
+			_ = r.b.ShrinkTo(n)
+			r.b.Commit(n)
+
+			seq, _, payload := decode()
+
+			if r.first {
+				if seq != 1 {
+					panic(fmt.Errorf(
+						"first packet sequence number = %d != 1",
+						seq,
+					))
+				}
+				r.first = false
+			}
+
+			start := time.Now()
+			discarded := r.proc.Process(seq, payload, r.b)
+			diff := time.Since(start).Microseconds()
+
+			if r.sofar < *samples {
+				if discarded > 0 {
+					log.Printf(
+						"discarded %d from buffer time=%d",
+						discarded,
+						diff,
+					)
+				}
+
+				_ = r.hist.RecordValue(diff)
+				r.sofar++
+			} else {
+				PrintHistogram(r.hist, r.proc.Buffered())
+				r.hist.Reset()
+
+				r.sofar = 0
+			}
+
+			if slice := r.b.ClaimFixed(r.readBufSize); slice != nil {
+				r.peer.AsyncRead(slice, onRead)
+			} else {
+				panic("out of buffer space")
+			}
+		}
+	}
+
+	r.peer.AsyncRead(r.b.ClaimFixed(r.readBufSize), onRead)
+}
---
+++ examples/sequencing/receiver/simple_proc.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"log"
+	"sort"
+
+	"github.com/talostrading/sonic"
+)
+
+var _ ByteBufferProcessor = &SimpleProcessor{}
+
+type SimpleProcessor struct {
+	expected int
+	buffer   []struct {
+		seq int
+		b   []byte
+	}
+}
+
+func NewSimpleProcessor() *SimpleProcessor {
+	p := &SimpleProcessor{expected: 1}
+	log.Println("using simple processor")
+	return p
+}
+
+func (p *SimpleProcessor) Process(
+	seq int,
+	payload []byte,
+	b *sonic.ByteBuffer,
+) int {
+	defer b.Consume(len(b.Data()))
+
+	// Ignore anything we received before.
+	if seq < p.expected {
+		if *debug && *verbose {
+			log.Printf(
+				"ignoring seq=%d(%d) as we are on %d n_bufferd=%d",
+				seq,
+				len(payload),
+				p.expected,
+				len(p.buffer),
+			)
+		}
+
+		return 0
+	}
+
+	if seq == p.expected {
+		if *debug {
+			log.Printf(
+				"processing live seq=%d(%d) n_buffered=%d",
+				seq,
+				len(payload),
+				len(p.buffer),
+			)
+		}
+
+		p.expected++
+		p.walkBuffer()
+	} else {
+		buffered := p.addToBuffer(seq, payload)
+		if buffered && *debug {
+			log.Printf(
+				"buffering seq=%d(%d) n_buffered=%d",
+				seq,
+				len(payload),
+				len(p.buffer),
+			)
+		}
+	}
+
+	return 0
+}
+
+func (p *SimpleProcessor) addToBuffer(seq int, payload []byte) (buffered bool) {
+	i := sort.Search(len(p.buffer), func(i int) bool {
+		return p.buffer[i].seq >= seq
+	})
+
+	if i >= len(p.buffer) {
+		b := make([]byte, len(payload))
+		copy(b, payload)
+
+		p.buffer = append(p.buffer, struct {
+			seq int
+			b   []byte
+		}{
+			seq,
+			b,
+		})
+
+		buffered = true
+	} else if i < len(p.buffer) && p.buffer[i].seq != seq {
+		b := make([]byte, len(payload))
+		copy(b, payload)
+
+		p.buffer = append(p.buffer[:i+1], p.buffer[i:]...)
+		p.buffer[i] = struct {
+			seq int
+			b   []byte
+		}{
+			seq,
+			b,
+		}
+		buffered = true
+	}
+
+	return buffered
+}
+
+func (p *SimpleProcessor) walkBuffer() {
+	var newBuffer []struct {
+		seq int
+		b   []byte
+	}
+	for _, entry := range p.buffer {
+		if entry.seq == p.expected {
+			if *debug {
+				log.Printf(
+					"processing buffered seq=%d n_buffered=%d",
+					p.expected,
+					len(p.buffer),
+				)
+			}
+
+			p.expected++
+		} else {
+			newBuffer = append(newBuffer, entry)
+		}
+	}
+	p.buffer = newBuffer
+}
+
+func (p *SimpleProcessor) Buffered() int {
+	return len(p.buffer)
+}
