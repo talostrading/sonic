c1657a8d6c5308b78759e9fbd584be38db0ad38d
sergiu128 <sergiu4096@gmail.com>
2023-07-24 11:11:01 +0200

    Do not commit stale sequence number in bip_buffer_reader

--- examples/sequencing/receiver/bip_buffer_reader.go
+++ examples/sequencing/receiver/bip_buffer_reader.go
@@ -48,39 +48,49 @@ func NewBipBufferReader(
 }
 
 func (r *BipBufferReader) Setup() {
-	var onRead func(error, int, netip.AddrPort)
-	onRead = func(err error, n int, _ netip.AddrPort) {
+	var (
+		b      []byte
+		onRead func(error, int, netip.AddrPort)
+	)
+
+	onRead = func(err error, nRead int, _ netip.AddrPort) {
 		if err != nil {
 			panic(err)
 		} else {
-			r.buf.Commit(n)
-			entire := r.buf.Data()
-			b := entire[len(entire)-r.readbufsize:]
+			seq, payloadSize, payload := r.decode(b)
+			if seq >= r.expected {
+				// We only want to store expected or future sequence numbers.
+				// Older ones are pointless to store as they will be Consumed
+				// later on in process. However, consuming them also takes time,
+				// so we're better off filtering them here.
+				r.buf.Commit(nRead)
 
-			seq, n, payload := r.decode(b)
+				entire := r.buf.Data()
+				b = entire[len(entire)-r.readbufsize:]
 
-			if r.first {
-				if seq != 1 {
-					panic(fmt.Errorf(
-						"first packet sequence number = %d != 1",
-						seq,
-					))
+				if r.first {
+					if seq != 1 {
+						panic(fmt.Errorf(
+							"first packet sequence number = %d != 1",
+							seq,
+						))
+					}
+					r.first = false
 				}
-				r.first = false
+
+				start := time.Now()
+				r.process(seq, payloadSize, payload)
+				diff := time.Since(start).Microseconds()
+
+				if r.hist.TotalCount() < int64(*samples) {
+					_ = r.hist.RecordValue(diff)
+				} else {
+					PrintHistogram(r.hist, r.buf.Committed()/r.readbufsize)
+					r.hist.Reset()
+				}
 			}
 
-			start := time.Now()
-			r.process(seq, n, payload)
-			diff := time.Since(start).Microseconds()
-
-			if r.hist.TotalCount() < int64(*samples) {
-				_ = r.hist.RecordValue(diff)
-			} else {
-				PrintHistogram(r.hist, r.buf.Committed()/r.readbufsize)
-				r.hist.Reset()
-			}
-
-			if b := r.buf.Claim(r.readbufsize); b != nil {
+			if b = r.buf.Claim(r.readbufsize); b != nil {
 				r.peer.AsyncRead(b, onRead)
 			} else {
 				panic("buffer full")
@@ -88,7 +98,8 @@ func (r *BipBufferReader) Setup() {
 		}
 	}
 
-	r.peer.AsyncRead(r.buf.Claim(r.readbufsize), onRead)
+	b = r.buf.Claim(r.readbufsize)
+	r.peer.AsyncRead(b, onRead)
 }
 
 func (r *BipBufferReader) decode(b []byte) (seq, n int, payload []byte) {
