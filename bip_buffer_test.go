85b1eb24c40eeb8c32dcd52985c9a19cd4128f70
sergiu128 <sergiu4096@gmail.com>
2023-07-25 10:33:20 +0200

    BipBuffer returns the committed chunk

--- bip_buffer_test.go
+++ bip_buffer_test.go
@@ -66,14 +66,17 @@ func TestBipBufferCommit(t *testing.T) {
 		t.Fatal("wrong committed")
 	}
 	if buf.Claimed() != 3 {
-		t.Fatal("wrong committed")
+		t.Fatal("wrong claimed")
 	}
-	buf.Commit(3)
+	b = buf.Commit(3)
 	if buf.Committed() != 3 {
 		t.Fatal("wrong committed")
 	}
-	if buf.Claimed() != 0 {
+	if !bytes.Equal(b, []byte{7, 22, 218}) {
 		t.Fatal("wrong committed")
+	}
+	if buf.Claimed() != 0 {
+		t.Fatal("wrong claimed")
 	}
 	b = buf.Data()
 	if !bytes.Equal(b, []byte{7, 22, 218}) {
@@ -99,7 +102,10 @@ func TestBipBufferConsume(t *testing.T) {
 		b[2] = 218
 		b[3] = 56
 	}
-	buf.Commit(4)
+	b := buf.Commit(4)
+	if !bytes.Equal(b, []byte{7, 22, 218, 56}) {
+		t.Fatal("wrong committed")
+	}
 	buf.Consume(2)
 	{
 		b := buf.Data()
@@ -125,7 +131,10 @@ func TestBipBufferClaimAfterWrapping(t *testing.T) {
 		b[2] = 218
 		b[3] = 56
 	}
-	buf.Commit(4)
+	b := buf.Commit(4)
+	if !bytes.Equal(b, []byte{7, 22, 218, 56}) {
+		t.Fatal("wrong committed")
+	}
 	buf.Consume(2)
 	{
 		b := buf.Claim(4)
@@ -135,7 +144,10 @@ func TestBipBufferClaimAfterWrapping(t *testing.T) {
 		b[0] = 49
 		b[1] = 81
 	}
-	buf.Commit(2)
+	b = buf.Commit(2)
+	if !bytes.Equal(b, []byte{49, 81}) {
+		t.Fatal("wrong committed")
+	}
 	{
 		b := buf.Data()
 		if !bytes.Equal(b, []byte{218, 56}) {
@@ -163,7 +175,10 @@ func TestBipBufferReset(t *testing.T) {
 	if buf.Claimed() != 4 {
 		t.Fatal("wrong claimed")
 	}
-	buf.Commit(4)
+	b := buf.Commit(4)
+	if !bytes.Equal(b, []byte{7, 22, 218, 56}) {
+		t.Fatal("wrong committed")
+	}
 	if buf.Claimed() != 0 {
 		t.Fatal("wrong claimed")
 	}
@@ -196,6 +211,54 @@ func TestBipBufferClaimSameChunk(t *testing.T) {
 		b := buf.Claim(4)
 		if !bytes.Equal(b, []byte{7 + 1, 22 + 1, 218 + 1, 56 + 1}) {
 			t.Fatal("wrong claim")
+		}
+	}
+}
+
+func TestBipBufferCommitAfterWrapping(t *testing.T) {
+	buf := NewBipBuffer(4)
+	{
+		b := buf.Claim(3)
+		b[0] = 7
+		b[1] = 22
+		b[2] = 218
+
+		b = buf.Commit(3)
+		if !bytes.Equal(b, []byte{7, 22, 218}) {
+			t.Fatal("wrong committed")
+		}
+	}
+	{
+		// overclaim and overcommit by 1
+		b := buf.Claim(2)
+		if len(b) != 1 {
+			t.Fatal("wrong claim")
+		}
+		b[0] = 56
+
+		b = buf.Commit(2)
+		if !bytes.Equal(b, []byte{56}) {
+			t.Fatal("wrong committed")
+		}
+	}
+	{
+		// wrap around
+		buf.Consume(3)
+		b := buf.Claim(3)
+		if !bytes.Equal(b, []byte{7, 22, 218}) {
+			t.Fatal("wrong committed")
+		}
+		b[0] = 2
+		b[1] = 3
+		b[2] = 5
+
+		b = buf.Commit(3)
+		if !bytes.Equal(b, []byte{2, 3, 5}) {
+			t.Fatal("wrong committed")
+		}
+
+		if !bytes.Equal(buf.Data(), []byte{56}) {
+			t.Fatal("wrong data")
 		}
 	}
 }
