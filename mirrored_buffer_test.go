91b24a0a445fa54602668f59d2318dce6d0a5b5b
sergiu128 <sergiu128@protonmail.com>
2023-09-10 20:10:59 +0200

    Add MirroredBuffer Commit and Consume

--- mirrored_buffer_test.go
+++ mirrored_buffer_test.go
@@ -7,13 +7,51 @@ import (
 
 func TestMirroredBuffer(t *testing.T) {
 	size := syscall.Getpagesize()
+
 	buf, err := NewMirroredBuffer(size)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	b := buf.Claim(size)
-	if b == nil {
-		t.Fatal("should claim")
+	n := size/2 + 1
+	b := buf.Claim(n)
+	if len(b) != n {
+		t.Fatal("wrong claim")
+	}
+
+	for i := range b {
+		b[i] = 42
+	}
+	if buf.Commit(n) != n {
+		t.Fatal("wrong commit")
+	}
+
+	if buf.Consume(n-1) != n-1 {
+		t.Fatal("wrong consume")
+	}
+	if buf.UsedSpace() != 1 {
+		t.Fatal("wrong used space")
+	}
+
+	if buf.head >= buf.tail {
+		t.Fatal("buffer should not be wrapped")
+	}
+
+	// The next slice will cross the mirror boundary.
+	n = buf.FreeSpace() - 1
+	b = buf.Claim(n)
+	if len(b) != n {
+		t.Fatal("wrong claim")
+	}
+	for i := range b {
+		b[i] = 84
+	}
+
+	if buf.Commit(n) != n {
+		t.Fatal("wrong claim")
+	}
+	
+	if buf.head <= buf.tail {
+		t.Fatal("buffer should be wrapped")
 	}
 }
