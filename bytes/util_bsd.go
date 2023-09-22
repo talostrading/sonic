21aea21f29ad26243091c6e95e94ea2bfc5f8e26
sergiu128 <sergiu128@protonmail.com>
2023-09-22 16:00:49 +0200

    MirroredBuffer: cleanup some more

--- bytes/util_bsd.go
+++ bytes/util_bsd.go
@@ -4,17 +4,14 @@ package bytes
 
 import "syscall"
 
-func mmapAllocate(size int, _ bool) ([]byte, error) {
-	// Reserve 2 * size of the process' virtual memory space.
+func mmapAllocate(size int, prefault bool) ([]byte, error) {
+	// Reserve `size` of the process' virtual memory space.
 	//
 	// This call is needed for its return value, which is a valid mapping
 	// address. We use this address with MAP_FIXED to mirror our desired buffer
 	// below.
 	//
-	// If prefault is true, this call also allocates physical memory. This will
-	// be visible in the process' resident memory.
-	// If prefault is false, no physical memory allocation occurs. Instead, we
-	// defer to lazy allocation of pages in RAM through on-demand paging.
+	// Prefault is ignored on BSD systems.
 
 	// No file backing, implies -1 as fd.
 	flags := syscall.MAP_ANON
@@ -23,21 +20,14 @@ func mmapAllocate(size int, _ bool) ([]byte, error) {
 	// are not carried through to the underlying file (which is inexistent,
 	// hence -1 as fd).
 	//
-	// Another effect of MAP_PRIVATE is that another mmap call mapping the same
-	// virtual address space will not see the changes made to the memory by the
-	// first mapping. Under the hood, this is achieved through kernel
-	// copy-on-write, which is the default behaviour for MAP_PRIVATE mappings.
-	// That means that if the first mapping tries to modify a page shared with
-	// another mapping, the page is first copied and then changed. The first
-	// mapping will hold the modified copy, the second mapping will refer the
-	// initial page. See the MapPrivate example in tests on how this works.
-	// TODO write that example.
+	// See TestMirroredBufferMmapBehaviour for an explanation of MAP_PRIVATE and
+	// MAP_SHARED.
 	flags |= syscall.MAP_PRIVATE
 
 	return syscall.Mmap(
-		-1,                // fd
-		0,                 // offset
-		size,              // size
+		-1,
+		0,
+		size,
 		syscall.PROT_NONE, // we can't do anything with this mapping
 		flags,
 	)
