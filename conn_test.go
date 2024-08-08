4b2f264a4ea6f0a76e740b327d83205978953cfd
sergiu128 <sergiu4096@gmail.com>
2024-08-08 23:44:28 +0200

    Merge conn2_test.go with conn_test.go

--- conn_test.go
+++ conn_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/talostrading/sonic/internal"
 	"github.com/talostrading/sonic/sonicopts"
 )
 
@@ -348,4 +349,56 @@ func TestConnWriteHandlesError(t *testing.T) {
 	if !done {
 		t.Fatal("test did not run to completion")
 	}
+}
+
+func TestTCPClientDefaultOpts(t *testing.T) {
+	mark := make(chan struct{}, 1)
+	defer func() { <-mark }()
+	go func() {
+		ln, err := net.Listen("tcp", "localhost:10100")
+		if err != nil {
+			panic(err)
+		}
+		defer func() {
+			ln.Close()
+			mark <- struct{}{}
+		}()
+		mark <- struct{}{}
+
+		conn, err := ln.Accept()
+		if err != nil {
+			panic(err)
+		}
+		defer conn.Close()
+
+		<-mark
+	}()
+	<-mark
+
+	ioc := MustIO()
+	defer ioc.Close()
+
+	conn, err := Dial(ioc, "tcp", "localhost:10100")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+
+	nonblocking, err := internal.IsNonblocking(conn.RawFd())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !nonblocking {
+		t.Fatal("sonic conn should be nonblocking by default")
+	}
+
+	noDelay, err := internal.IsNoDelay(conn.RawFd())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if noDelay {
+		t.Fatal("sonic tcp conn should not have Nagle's algorithm off by default")
+	}
+
+	mark <- struct{}{}
 }
