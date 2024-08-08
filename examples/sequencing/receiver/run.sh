52330083034f508c68bafbe55736a3471fb9e274
sergiu128 <sergiu4096@gmail.com>
2024-08-08 23:18:06 +0200

    Delete incomplete examples

--- examples/sequencing/receiver/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-GOMEMLIMIT=2750MiB GODEBUG=asyncpreemptoff=1 strace -e trace=memory ./recv -slow=false -iter=16384
--- examples/websocket/server.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func main() {
-	panic("implement me")
-}
