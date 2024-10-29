a6ed38a83c882e046f3470bd140d4514c748035f
sergiu128 <sergiu.marin@talostrading.com>
2024-10-29 10:11:41 +0100

    [websocket] Track IO latency in binance example

--- examples/binance/main.go
+++ examples/binance/main.go
@@ -2,24 +2,34 @@ package main
 
 import (
 	"crypto/tls"
+	"flag"
 	"fmt"
+	"time"
 
 	"github.com/talostrading/sonic"
 	"github.com/talostrading/sonic/codec/websocket"
+	"github.com/talostrading/sonic/util"
 )
 
-var subscriptionMessage = []byte(
-	`
+var (
+	verbose = flag.Bool("v", false, "if true, websocket messages are printed")
+
+	subscriptionMessage = []byte(`
 {
   "id": 1,
   "method": "SUBSCRIBE",
   "params": [ "bnbbtc@depth" ]
 }
 `)
+)
 
 func main() {
+	flag.Parse()
+
 	ioc := sonic.MustIO()
 	defer ioc.Close()
+
+	ioLatency := util.NewOnlineStats()
 
 	stream, err := websocket.NewWebsocketStream(ioc, &tls.Config{}, websocket.RoleClient)
 	if err != nil {
@@ -45,12 +55,43 @@ func main() {
 					panic(err)
 				}
 
-				fmt.Println(string(b[:n]))
+				if *verbose {
+					fmt.Println(string(b[:n]))
+				}
 				stream.AsyncNextMessage(b[:], onRead)
 			}
 			stream.AsyncNextMessage(b[:], onRead)
 		})
 	})
 
-	ioc.Run()
+	eventsReceived := 0
+
+	ioLatencyTimer, err := sonic.NewTimer(ioc)
+	if err != nil {
+		panic(err)
+	}
+	ioLatencyTimer.ScheduleRepeating(time.Second, func() {
+		if eventsReceived > 0 {
+			result := ioLatency.Result()
+			fmt.Printf(
+				"min/avg/max/stddev = %.2f/%.2f/%.2f/%.2f us from %d events\n",
+				result.Min,
+				result.Avg,
+				result.Max,
+				result.StdDev,
+				eventsReceived,
+			)
+			ioLatency.Reset()
+			eventsReceived = 0
+		}
+	})
+
+	for {
+		start := time.Now()
+		n, _ := ioc.PollOne()
+		if n > 0 {
+			eventsReceived += n
+			ioLatency.Add(float64(time.Now().Sub(start).Microseconds()))
+		}
+	}
 }
