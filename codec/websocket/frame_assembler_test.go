7da1a3ecc10ebcb41ca81cd7060c090564c0f62c
Max Maeder <max.s.maeder@gmail.com>
2025-01-09 13:27:56 -0600

    Clean up frame assembler test

--- codec/websocket/frame_assembler_test.go
+++ codec/websocket/frame_assembler_test.go
@@ -8,29 +8,35 @@ import (
 )
 
 func TestFrameAssemblerEmpty(t *testing.T) {
+	assert := assert.New(t)
+
 	assembler := NewFrameAssembler()
-	assert.NotNil(t, assembler)
+	assert.NotNil(assembler)
 
 	slices := assembler.Slices()
-	assert.Len(t, slices, 0)
+	assert.Len(slices, 0)
 
 	reassembled := assembler.Reassemble()
-	assert.Equal(t, 0, len(reassembled))
+	assert.Equal(0, len(reassembled))
 }
 
 func TestFrameAssemblerSingle(t *testing.T) {
+	assert := assert.New(t)
+
 	payload := []byte{0x01, 0x02, 0x03}
 	assembler := NewFrameAssembler(payload)
 
 	slices := assembler.Slices()
-	assert.Len(t, slices, 1)
-	assert.Equal(t, payload, slices[0])
+	assert.Len(slices, 1)
+	assert.Equal(payload, slices[0])
 
 	reassembled := assembler.Reassemble()
-	assert.Equal(t, payload, reassembled)
+	assert.Equal(payload, reassembled)
 }
 
 func TestFrameAssemblerMulti(t *testing.T) {
+	assert := assert.New(t)
+
 	payload1 := []byte{0x01, 0x02}
 	payload2 := []byte{0x03, 0x04, 0x05}
 	payload3 := []byte{0x06}
@@ -38,24 +44,26 @@ func TestFrameAssemblerMulti(t *testing.T) {
 	assembler := NewFrameAssembler(payload1, payload2, payload3)
 
 	slices := assembler.Slices()
-	assert.Len(t, slices, 3)
-	assert.Equal(t, payload1, slices[0])
-	assert.Equal(t, payload2, slices[1])
-	assert.Equal(t, payload3, slices[2])
+	assert.Len(slices, 3)
+	assert.Equal(payload1, slices[0])
+	assert.Equal(payload2, slices[1])
+	assert.Equal(payload3, slices[2])
 
 	reassembled := assembler.Reassemble()
 	want := bytes.Join([][]byte{payload1, payload2, payload3}, nil)
-	assert.Equal(t, want, reassembled)
+	assert.Equal(want, reassembled)
 }
 
 func TestFrameAssemblerAppend(t *testing.T) {
+	assert := assert.New(t)
+
 	assembler := NewFrameAssembler([]byte{0x01}, []byte{0x02})
 	assembler.Append([]byte{0x03, 0x04})
 
 	slices := assembler.Slices()
-	assert.Len(t, slices, 3)
+	assert.Len(slices, 3)
 
 	want := []byte{0x01, 0x02, 0x03, 0x04}
 	reassembled := assembler.Reassemble()
-	assert.Equal(t, want, reassembled)
+	assert.Equal(want, reassembled)
 }
